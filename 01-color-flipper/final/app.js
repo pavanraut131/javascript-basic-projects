d69d7ff90b13af638b51d5c78620636b445cd159
bhanudevc <bhanudevchauhan@gmail.com>
2025-01-30 13:55:22 +0530

    Revert "color will be reverted back"
    This reverts commit 6b83a6a2f5710167486fe134740dd49880770ba7.

--- 01-color-flipper/final/app.js
+++ 01-color-flipper/final/app.js
@@ -1,13 +1,13 @@
 const colors = ["green", "red", "rgba(133,122,200)", "#f15025"];
 const btn = document.getElementById("btn");
-const color = document.querySelector(".color");
+const clr = document.querySelector(".color");
 
 btn.addEventListener("click", function () {
   const newnumber = generateRandomNumber();
   // console.log(randomNumber);
 
   document.body.style.backgroundColor = colors[newnumber];
-  color.textContent = colors[newnumber];
+  clr.textContent = colors[newnumber];
 });
 
 function generateRandomNumber() {
